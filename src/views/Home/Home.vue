c8cc8c9d5ed7a4f6feba6d74e8f3b5915ca5c7d1
NewCicada <1547598968@qq.com>
2023-01-24 13:21:14 +0800

    顶栏Tab完成

---
+++ src/views/Home/Home.vue
@@ -0,0 +1,15 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import AppHeader from '@/components/app-header.vue'
+
+
+</script>
+
+<template>
+    <AppHeader></AppHeader>
+
+</template>
+
+<style scoped>
+
+</style>
